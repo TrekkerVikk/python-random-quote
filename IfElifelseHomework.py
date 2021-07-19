ca0dc7dde1202885731a133e7871dbaf3c257f5f
TrekkerVikk <76450982+TrekkerVikk@users.noreply.github.com>
2021-07-19 15:29:23 +0200

    Add files via upload

---
+++ IfElifelseHomework.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# if condition : 
+
+
+# In[7]:
+
+
+if True:
+    print("It worked")
+    
+
+
+# In[8]:
+
+
+num1 = 100
+num2 = 50
+
+if num1 > num2:
+    print("num1 is bigger than num2")
+
+
+# In[15]:
+
+
+num1 = 100
+num2 = 50
+
+if num1 > num2:
+    print("num1 is bigger than num2")
+else:
+    print("num2 is bigger than num1")
+
+
+# In[16]:
+
+
+num1 = 100
+num2 = 150
+
+if num1 > num2:
+    print("num1 is bigger than num2")
+else:
+    print("num2 is bigger than num1")
+
+
+# In[20]:
+
+
+num1 = 100
+num2 = 100
+
+if num1 > num2:
+    print("num1 is bigger than num2")
+elif num2>num1:
+    print("num2 is bigger than num1")
+else:
+    print("Both numbers are equal")
+
+
+# In[2]:
+
+
+Vikram = 34
+Natasha = 31
+
+if Vikram > Natasha:
+    print("Vikram is elder than Natasha")
+elif Natasha>Vikram:
+    print("Natasha is younger than Vikram")
+else:
+    print("Both are of same age group")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ LoopsCommand1.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+number = 1
+
+while number <=15:
+    print(number)
+    number = number +1
+
+
+# In[10]:
+
+
+number = 1
+
+while number <=10:
+    if number % 2 !=0:
+        print(number)
+    number = number +1
+
+
+# In[14]:
+
+
+L = []
+
+while len(L)<5:
+    new_name = input("Please add a new name:").strip().capitalize()
+    L.append(new_name)
+    
+print("Sorry list is full")
+print(L)
+
+
+# In[ ]:
+
+
+while 2>1:
+    print('hello')
+
+
+# In[ ]:
+
+
+while True:
+    print("Hello")
+
+
+# In[ ]:
+
+
+
+
